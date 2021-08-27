786f34a601259ed127f558048c7a26a232be9540
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-27 14:18:43 +0900

    destroyアクション、ログアウト機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
   def new
     @user = User.new
   end
@@ -7,7 +8,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user.id)
-
     else
       render :new
     end
