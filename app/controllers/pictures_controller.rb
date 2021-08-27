cc1a9fb626ac536d0f53968937aeea89a9857f4d
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-27 16:21:38 +0900

    index投稿画像一覧画面作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,9 +1,21 @@
 class PicturesController < ApplicationController
 
   def index
+    @picture = Picture.all
   end
 
   def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:image, :content)
   end
 
 end
