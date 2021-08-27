2b1a4be13b85714fca90d1df0ff3fda4fbe93c71
atsushi oshima <aboutagial@yahoo.co.jp>
2021-08-27 23:49:47 +0900

    confirm登録前の確認画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
-  resources :pictures
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+  end
 end
